b7ee58ba202e0993051bc7513b4f7303a350f6b9
moustafa <moustafasaed060@gmail.com>
2025-01-01 11:39:43 +0200

    activeRates, localStorage Handeling

--- js/main.js
+++ js/main.js
@@ -1,16 +1,37 @@
 const rates = document.querySelectorAll(".rate-nmbrs a");
 const sbmt = document.querySelector(".sbmt");
+const rateValue = window.localStorage.getItem("rate");
+
+function deleteActiveClass(arr) {
+    arr.forEach(el => {
+        el.classList.remove("active")
+    })
+}
+
+if(rateValue) {
+    // console.log(rateValue);
+    deleteActiveClass(rates);
+    rates.forEach(rate => {
+        if(rate.getAttribute("data-ratee") == rateValue) {
+            console.log(rate)
+            rate.classList.add("active");
+        }
+    })
+} else {
+    console.log("not local  ...  ");
+}
 
 var dataRateValue = "";
 rates.forEach(rate => {
     rate.addEventListener("click", (e)=>{
         e.preventDefault();
         dataRateValue = rate.getAttribute("data-ratee");
-        console.log(dataRateValue);
+        deleteActiveClass(rates);
+        e.target.classList.add("active");
+        // console.log(dataRateValue);
         window.localStorage.setItem("rate",dataRateValue);
     })
 });
-
 
 
 sbmt.addEventListener("click", ()=>{
